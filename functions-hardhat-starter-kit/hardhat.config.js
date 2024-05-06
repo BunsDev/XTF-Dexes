085fd99b4ac6a42101feb4d1e899218356667b7b
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-05-06 15:29:04 +0100

    feat: cloned and added chainlink functions starter kit

---
+++ functions-hardhat-starter-kit/hardhat.config.js
@@ -0,0 +1,114 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-contract-sizer")
+require("./tasks")
+const { networks } = require("./networks")
+
+// Enable gas reporting (optional)
+const REPORT_GAS = process.env.REPORT_GAS?.toLowerCase() === "true" ? true : false
+
+const SOLC_SETTINGS = {
+  optimizer: {
+    enabled: true,
+    runs: 1_000,
+  },
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "localFunctionsTestnet",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.8.7",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.7.0",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.6.6",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.4.24",
+        settings: SOLC_SETTINGS,
+      },
+    ],
+  },
+  networks: {
+    ...networks,
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: networks.ethereum.verifyApiKey,
+      avalanche: networks.avalanche.verifyApiKey,
+      polygon: networks.polygon.verifyApiKey,
+      sepolia: networks.ethereumSepolia.verifyApiKey,
+      polygonMumbai: networks.polygonMumbai.verifyApiKey,
+      polygonAmoy: networks.polygonAmoy.verifyApiKey,
+      avalancheFujiTestnet: networks.avalancheFuji.verifyApiKey,
+      arbitrum: networks.arbitrum.verifyApiKey,
+      arbitrumSepolia: networks.arbitrumSepolia.verifyApiKey,
+      baseSepolia: networks.baseSepolia.verifyApiKey,
+      optimismSepolia: networks.optimismSepolia.verifyApiKey,
+    },
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia-explorer.base.org",
+        },
+      },
+      {
+        network: "optimismSepolia",
+        chainId: 11155420,
+        urls: {
+          apiURL: "https://api-sepolia-optimistic.etherscan.io/api", // https://docs.optimism.etherscan.io/v/optimism-sepolia-etherscan
+          browserURL: "https://sepolia-optimistic.etherscan.io/",
+        },
+      },
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com",
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: REPORT_GAS,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  contractSizer: {
+    runOnCompile: false,
+    only: ["FunctionsConsumer", "AutomatedFunctionsConsumer", "FunctionsBillingRegistry"],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./build/cache",
+    artifacts: "./build/artifacts",
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+}
