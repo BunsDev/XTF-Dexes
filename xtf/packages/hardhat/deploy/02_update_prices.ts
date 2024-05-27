b55e57327cb78a0be092ab9abb541d6bbceeaa0a
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-05-27 13:18:47 +0100

    feat: added script for fecthing prices

--- xtf/packages/hardhat/deploy/02_update_prices.ts
+++ xtf/packages/hardhat/deploy/02_update_prices.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { IndexAggregator, IndexCentralisedData } from "../typechain-types";
+import { IndexAggregator } from "../typechain-types";
 import { networks } from "../scripts/networks";
 import { SubscriptionManager, SecretsManager, Location } from "@chainlink/functions-toolkit";
 import { JsonRpcProvider } from "@ethersproject/providers";
@@ -17,6 +17,8 @@ const tokenDetails = new Map<string, any>();
 
 const tokenInfo = [] as any[];
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 // import { config } from "@chainlink/env-enc";
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -31,111 +33,122 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     const network = networks["ethereumSepolia"];
     const sepoliaChainId = 11155111;
 
-    // const marketArray = Object.values(markets);
-    // const categoryArray = Object.values(category).slice(0, 1);
-
-    // get the markets hitting the coingecko API https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd
-    // and save it in markets object
-
     const example = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd");
 
     const markets = await example.json();
 
-    // console.log("mm\n", markets);
+    const indexAggregator = await hre.ethers.getContract<IndexAggregator>("IndexAggregator");
 
-    const indexCentralisedData = await hre.ethers.getContract<IndexAggregator>("IndexAggregator");
+    const tokenNumber = 10;
+    // const decimals = 8;
 
-    const symbolNumber = 10;
-    const decimals = 8;
+    // for (let i = 0; i < tokenNumber; i++) {
+    //   const [symbol, tokenAddress, chainId, priceAggregator] = await indexAggregator.tokenInfo(i);
+    //   // console.log("symbol", symbol, "tokenAddress", tokenAddress, "chainId", chainId, "priceAggregator", priceAggregator);
+    //   // get contract MockAggregator at priceAggregator address
+    //   const mockAggregator = await hre.ethers.getContractAt("MockAggregator", priceAggregator);
 
-    for (let i = 0; i < symbolNumber; i++) {
-      const [symbol, tokenAddress, chainId, priceAggregator] = await indexCentralisedData.tokenInfo(i);
-      // console.log("symbol", symbol, "tokenAddress", tokenAddress, "chainId", chainId, "priceAggregator", priceAggregator);
-      // get contract MockAggregator at priceAggregator address
-      const mockAggregator = await hre.ethers.getContractAt("MockAggregator", priceAggregator);
+    //   // get the price of the token by matching the symbol with the markets object
+    //   const price = markets.find((market: any) => market.symbol === symbol)?.current_price;
+    //   const supply = markets.find((market: any) => market.symbol === symbol)?.circulating_supply;
 
-      // get the price of the token by matching the symbol with the markets object
-      const price = markets.find((market: any) => market.symbol === symbol)?.current_price;
+    //   const currentPrice = BigNumber.from(String(price).replace(".", "")).mul(
+    //     BigNumber.from("10").pow(decimals - (String(price).split(".")[1]?.length || 0)),
+    //   );
 
-      const currentPrice = BigNumber.from(String(price).replace(".", "")).mul(
-        BigNumber.from("10").pow(decimals - (String(price).split(".")[1]?.length || 0)),
-      );
+    //   const circulatingSupply = BigNumber.from(String(supply).replace(".", "")).mul(
+    //     BigNumber.from("10").pow(decimals - (String(supply).split(".")[1]?.length || 0)),
+    //   );
+    //   console.log(
+    //     "price for",
+    //     symbol,
+    //     "found",
+    //     currentPrice.toString(),
+    //     "at",
+    //     await mockAggregator.getAddress(),
+    //     "supply",
+    //     circulatingSupply.toString(),
+    //   );
 
-      console.log("price for", symbol, "found", currentPrice.toString(), "at", await mockAggregator.getAddress());
-
-      // set the price in the MockAggregator contract
-      // await mockAggregator.setPrice(currentPrice.toString());
-
-      // await mockAggregator.setPrice(100);
-    }
-
-    // for (let i = 0; i < categoryArray.length; i++) {
-    //   const category = categoryArray[i];
-    //   const categoryID = category?.id;
-
-    //   // load the file at `../../../../coingecko/categories/${categoryID}.json`
-    //   // and parse it]
-
-    //   if (categoryID !== undefined) {
-    //     categoryObject.set(categoryID, []);
-    //   }
-
-    //   try {
-    //     const categoryData = await fs.readFileSync(
-    //       path.resolve(__dirname, `../../../../coingecko/categories/${categoryID}.json`),
-    //       "utf8",
-    //     );
-
-    //     const categoryDataJSON = JSON.parse(categoryData);
-    //     for (let j = 0; j < categoryDataJSON.length; j++) {
-    //       const market = categoryDataJSON[j];
-    //       const symbol = market?.symbol;
-    //       const priceObject = marketArray.find(m => m.symbol === symbol);
-    //       if (priceObject !== undefined) {
-    //         categoryObject.get(categoryID)?.push(symbol);
-    //         if (tokenDetails.get(symbol) === undefined) {
-    //           const decimals = 8;
-    //           const circulatingSupply = BigNumber.from(String(priceObject.circulating_supply).replace(".", "")).mul(
-    //             BigNumber.from("10").pow(
-    //               decimals - (String(priceObject.circulating_supply).split(".")[1]?.length || 0),
-    //             ),
-    //           );
-    //           console.log("PRICEEE", circulatingSupply);
-
-    //           tokenDetails.set(symbol, priceObject);
-    //           console.log(symbol, priceObject.current_price, priceObject.circulating_supply, priceObject.name);
-    //           await deploy("SimpleERC20", {
-    //             from: deployer,
-    //             args: [priceObject.name, priceObject.symbol, circulatingSupply],
-    //             log: true,
-    //           });
-
-    //           const currentPrice = BigNumber.from(String(priceObject.current_price).replace(".", "")).mul(
-    //             BigNumber.from("10").pow(decimals - (String(priceObject.current_price).split(".")[1]?.length || 0)),
-    //           );
-
-    //           await deploy("MockAggregator", {
-    //             from: deployer,
-    //             args: [currentPrice, decimals],
-    //             log: true,
-    //           });
-
-    //           const simpleERC20 = await hre.ethers.getContract("SimpleERC20");
-    //           const mockAggregator = await hre.ethers.getContract("MockAggregator");
-
-    //           tokenInfo.push({
-    //             _symbol: priceObject.symbol,
-    //             _address: await simpleERC20.getAddress(),
-    //             _chainId: sepoliaChainId,
-    //             _aggregator: await mockAggregator.getAddress(),
-    //           });
-    //         }
-    //       }
-    //     }
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
+    // set the price in the MockAggregator contract
+    // await mockAggregator.setPrice(currentPrice.toString());
     // }
+
+    const today = new Date();
+    // FORMAT DD-MM-YYYY
+    for (let dayNumber = 0; dayNumber < 7; dayNumber++) {
+      for (let i = 0; i < tokenNumber; i++) {
+        const todayString = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+        const [symbol, tokenAddress, chainId, priceAggregator] = await indexAggregator.tokenInfo(i);
+        // console.log(
+        //   "symbol",
+        //   symbol,
+        //   "tokenAddress",
+        //   tokenAddress,
+        //   "chainId",
+        //   chainId,
+        //   "priceAggregator",
+        //   priceAggregator,
+        // );
+        // get the latest price of the token
+        const mockAggregator = await hre.ethers.getContractAt("MockAggregator", priceAggregator);
+        const id = markets.find((market: any) => market.symbol === symbol)?.id;
+        // console.log(symbol, id, await mockAggregator.latestRoundData());
+        // match the symbol with the markets object id
+        const historyEndpoint = `https://api.coingecko.com/api/v3/coins/${id}/history?date=${todayString}`;
+        const history = await fetch(historyEndpoint);
+        const historyJSON = await history.json();
+        let price = historyJSON.market_data.current_price.usd;
+        const supply = historyJSON.market_data.circulating_supply;
+        const decimals = 8;
+        const currentDecimal = String(price).split(".")[1]?.length || 0;
+
+        // console.log("currentDecimal", currentDecimal);
+
+        price = String(price).replace(".", "");
+        if (currentDecimal < decimals) {
+          price += "0".repeat(decimals - currentDecimal);
+        } else if (currentDecimal > decimals) {
+          price = price.slice(0, price.length - (currentDecimal - decimals));
+        }
+        console.log(todayString, symbol, price);
+        await mockAggregator.setPrice(price);
+        await sleep(13500);
+      }
+      await indexAggregator.collectPriceFeeds();
+      today.setDate(today.getDate() - 1);
+    }
+    // collect the price feeds
+    // await indexAggregator.collectPriceFeeds();
+
+    //  now sort the tokens and persist them in the contract
+
+    // const samples = 2;
+    // const tokenAverages = [] as any[];
+    // // get the prices of the tokens for ordering
+    // for (let i = 0; i < tokenNumber; i++) {
+    //   const priceAverage = BigNumber.from(0);
+    //   const supply = await indexAggregator.totalSupplies(i);
+    //   for (let j = 0; j < samples; j++) {
+    //     const price = await indexAggregator.movingAverage(i, j);
+    //     priceAverage.add(BigNumber.from(price).mul(supply));
+    //   }
+    //   tokenAverages.push(priceAverage);
+    // }
+
+    // console.log(
+    //   "tokenAverages",
+    //   tokenAverages.map(price => price.toString()),
+    // );
+
+    // // sort the tokens descending
+    // const sortedTokenIndexes = tokenAverages
+    //   .map((price, index) => ({ price, index }))
+    //   .sort((a, b) => (a.price.gt(b.price) ? -1 : 1))
+    //   .map(({ index }) => index);
+    // console.log("sorted", sortedTokenIndexes);
+
+    // await indexAggregator.persistIndex(sortedTokenIndexes);
   }
 };
 
