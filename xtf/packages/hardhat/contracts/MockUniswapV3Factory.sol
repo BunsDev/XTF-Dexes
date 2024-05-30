436d541ac49f4a9c422003a8901ead5af9ea0160
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-05-30 16:07:18 +0100

    feat: added mock contracts for uniswap v4

---
+++ xtf/packages/hardhat/contracts/MockUniswapV3Factory.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.0;
+
+import "./IUniswapV3Factory.sol";
+import "./IUniswapV3Pool.sol";
+
+
+contract MockkUniswapV3Factory is IUniswapV3Factory {
+
+    address _owner;
+    mapping(address => mapping(address => mapping(uint24 => address))) public _pools;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+	function getPool(
+		address tokenA,
+		address tokenB,
+		uint24 fee
+	) external view override returns (address) {
+		return _pools[tokenA][tokenB][fee];
+	}
+
+	function owner() external view override returns (address) {
+        return _owner;
+    }
+
+	function feeAmountTickSpacing(
+		uint24 fee
+	) external view override returns (int24) {
+        return 0;
+    }
+
+	function createPool(
+		address tokenA,
+		address tokenB,
+		uint24 fee
+	) external override returns (address pool) {}
+
+	function setOwner(address _owner) external override {}
+
+	function enableFeeAmount(uint24 fee, int24 tickSpacing) external override {}
+}
---
+++ xtf/packages/hardhat/contracts/MockUniswapV3Pool.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+import "./IUniswapV3Pool.sol";
+
+contract MockUniswapV3Pool is IUniswapV3Pool {
+
+    address _token0;
+    address _token1;
+    uint128 _liquidity;
+    uint24 _fee;
+
+    constructor(
+        address t0,
+        address t1,
+        uint24 f,
+        uint128 l
+    ) {
+        _token0 = t0;
+        _token1 = t1;
+        _fee = f;
+        _liquidity = l;
+    }
+
+    function setLiquidity(uint128 l) external {
+        _liquidity = l;
+    }
+
+	function liquidity() external view override returns (uint128) {}
+
+	function token0() external view override returns (address) {}
+
+	function token1() external view override returns (address) {}
+}
