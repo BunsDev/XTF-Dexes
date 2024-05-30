82bb89a64c67f006bc44a440662054bfc7be58fd
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-05-30 15:38:11 +0100

    feat: test centralsied index

--- xtf/packages/hardhat/test/IndexCentralisedData.ts
+++ xtf/packages/hardhat/test/IndexCentralisedData.ts
@@ -5,18 +5,34 @@ import { defaultAbiCoder } from "@ethersproject/abi";
 
 describe("IndexCentralisedData", function () {
   let indexAggregator: IndexCentralisedData;
+  let routerAddress: string;
+  let donIdBytes32: string;
 
   before(async () => {
-    const [owner] = await ethers.getSigners();
+    // const [owner] = await ethers.getSigners();
     const indexCentralisedData = await ethers.getContractFactory("IndexCentralisedData");
     const donId = "fun-ethereum-mainnet-1";
-    const routerAddress = "0x65Dcc24F8ff9e51F10DCc7Ed1e4e2A61e6E14bd6";
-    const donIdBytes32 = ethers.encodeBytes32String(donId);
+    routerAddress = "0x65Dcc24F8ff9e51F10DCc7Ed1e4e2A61e6E14bd6";
+    donIdBytes32 = ethers.encodeBytes32String(donId);
 
     indexAggregator = await indexCentralisedData.deploy(routerAddress, donIdBytes32);
   });
 
   describe("Deployment", function () {
-    it("Should send request to the function consumer", async function () {});
+    it("Should send request to the function consumer", async function () {
+      const request = {
+        id: "1",
+        data: {
+          endpoint: "price",
+          network: "ethereum",
+          referenceContract: routerAddress,
+          referenceAggregator: donIdBytes32,
+        },
+        meta: {
+          aggregator: "IndexCentralisedData",
+        },
+      };
+    });
+    // indexAggregator.request(request);
   });
 });
