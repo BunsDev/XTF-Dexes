140f455cf1942cec4fa7d264cef0559c776ee8af
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-06-01 10:48:46 +0100

    feat: added switch to show all

--- xtf/packages/nextjs/app/indexes/page.tsx
+++ xtf/packages/nextjs/app/indexes/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-// import { useEffect, useState } from "react";
+import { useState } from "react";
 import * as young from "../../../../../coingecko/categories/young.json";
 import * as category from "../../../../../coingecko/category.json";
 import * as market from "../../../../../coingecko/market.json";
-import { Avatar, Card, Col, Divider, InputNumber, List, Row, Select, Skeleton, Tag } from "antd";
+import { Avatar, Card, Col, Divider, InputNumber, List, Row, Select, Switch, Tag } from "antd";
 import { Watermark } from "antd";
 import type { NextPage } from "next";
 
@@ -12,8 +12,9 @@ const { Group } = Avatar;
 const youngList = Object.values(young);
 
 const Debug: NextPage = () => {
+  const [showAll, setShowAll] = useState(false);
   const indexData = Object.values(market);
-  const indexLimit = 100;
+  const indexLimit = 20;
 
   return (
     <>
@@ -151,10 +152,8 @@ const Debug: NextPage = () => {
                 <Group maxCount={3} size="large" maxStyle={{ color: "#f56a00", backgroundColor: "#fde3cf" }}>
                   <Avatar src="https://yt3.googleusercontent.com/YSnvnM1YCBJCkRZkZlnfjHCstaEuYh9hVhGC30Yr4X8yKWZRNIjj1RZMdRz2o9GzhSn1ycNP=s900-c-k-c0x00ffffff-no-rj" />
                   <Avatar src="https://yt3.googleusercontent.com/GojVNnQxHXs5QQktrtrUq145i_p4zFnRuiriuS7y170sRtezL79Ke86DkIkiiBH7CnH0nKcCkg=s900-c-k-c0x00ffffff-no-rj" />
-                  <Avatar src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2RCExxal8BLDfwx7PH1VXZXS9YH9LW0YZUVS16P0JETXdWLSxOgMTOO8zyF66uvT37T8&usqp=CAU"/>
+                  <Avatar src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2RCExxal8BLDfwx7PH1VXZXS9YH9LW0YZUVS16P0JETXdWLSxOgMTOO8zyF66uvT37T8&usqp=CAU" />
                 </Group>
-                  
-                 
               }
             >
               <p
@@ -191,8 +190,7 @@ const Debug: NextPage = () => {
             <br />
           </Col>
         </Row>
-        <br />
-        <br />
+
         <Divider orientation="left"></Divider>
         <h1
           style={{
@@ -213,128 +211,140 @@ const Debug: NextPage = () => {
           ></Avatar>
           Powered by <b>ChainLink</b> Data Feeds
         </h2>
+        <Switch
+          checkedChildren="Show All Tokens"
+          unCheckedChildren="Show Categories"
+          checked={showAll}
+          onChange={setShowAll}
+          style={{
+            marginBottom: "1rem",
+          }}
+        />
         <br />
         <br />
 
-        <Row gutter={130}>
-          {category
-            // get the first 10 categories
-            .slice(0, 16)
-            .filter(c => c.content !== "")
-            .map(c => (
-              <Col span={8} key={c.name}>
-                <Card
-                  title={
-                    <a
-                      // remove also ( )
-                      // href={`/indexes/${c.name.toLowerCase().replace(/\(|\)/g, "").replace(/\s/g, "-")}`}
-                      href={`/indexes/${c.id}`}
-                      // target="_blank"
-                      rel="noopener noreferrer"
+        {!showAll && (
+          <Row gutter={130}>
+            {category
+              // get the first 10 categories
+              .slice(0, 16)
+              .filter(c => c.content !== "")
+              .map(c => (
+                <Col span={8} key={c.name}>
+                  <Card
+                    title={
+                      <a
+                        // remove also ( )
+                        // href={`/indexes/${c.name.toLowerCase().replace(/\(|\)/g, "").replace(/\s/g, "-")}`}
+                        href={`/indexes/${c.id}`}
+                        // target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {c.name}
+                      </a>
+                    }
+                    bordered={false}
+                    style={{
+                      height: "200px",
+                      overflow: "hidden",
+                    }}
+                    extra={
+                      <Group maxCount={3} size="large" maxStyle={{ color: "#f56a00", backgroundColor: "#fde3cf" }}>
+                        {c.top_3_coins.map((coin: any) => (
+                          <Avatar key={coin} src={coin} />
+                        ))}
+                      </Group>
+                    }
+                  >
+                    <p
+                      style={{
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        lineHeight: "1.2em",
+                        height: "6em",
+                        WebkitLineClamp: 5,
+                        whiteSpace: "pre-wrap",
+                        textAlign: "left",
+                      }}
                     >
-                      {c.name}
+                      {c.content}
+                    </p>
+                  </Card>
+                  <br />
+                </Col>
+              ))}
+          </Row>
+        )}
+        {showAll && (
+          <List
+            // className="demo-loadmore-list"
+            itemLayout="horizontal"
+            bordered
+            style={{
+              marginTop: "80px",
+              minHeight: "400px",
+              width: "800px",
+              margin: "auto",
+            }}
+            dataSource={
+              // meme is not an array, so we need to convert it to an array
+              Object.keys(indexData)
+                .map((k: any) => indexData[k])
+                .slice(0, indexLimit)
+            }
+            renderItem={item => (
+              <List.Item
+                actions={[
+                  <a key="list-loadmore-edit">{"Rank #" + item.market_cap_rank}</a>,
+                  <a key="list-loadmore-more">{"Lqdty: " + Math.random().toFixed(2) + "P"}</a>,
+                ]}
+              >
+                <List.Item.Meta
+                  avatar={
+                    <Avatar
+                      style={{
+                        width: "50px",
+                        height: "50px",
+                      }}
+                      src={item.image}
+                    />
+                  }
+                  title={
+                    <a href="https://ant.design">
+                      {
+                        <>
+                          {item.name}
+                          <Tag
+                            style={{
+                              marginLeft: "10px",
+                            }}
+                          >
+                            {youngList.find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
+                              ?.networks?.[0].Name || "Wrapped Ethereum (Sepolia)"}
+                          </Tag>
+                        </>
+                      }
                     </a>
                   }
-                  bordered={false}
-                  style={{
-                    height: "200px",
-                    overflow: "hidden",
-                  }}
-                  extra={
-                    <Group maxCount={3} size="large" maxStyle={{ color: "#f56a00", backgroundColor: "#fde3cf" }}>
-                      {c.top_3_coins.map((coin: any) => (
-                        <Avatar key={coin} src={coin} />
-                      ))}
-                    </Group>
+                  description={
+                    <span>
+                      {youngList.find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
+                        ?.descriptions?.en !== undefined
+                        ? youngList
+                            .find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
+                            ?.descriptions?.en?.slice(0, 100) + "..."
+                        : "No description available"}
+                    </span>
                   }
-                >
-                  <p
-                    style={{
-                      display: "-webkit-box",
-                      WebkitBoxOrient: "vertical",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      lineHeight: "1.2em",
-                      height: "6em",
-                      WebkitLineClamp: 5,
-                      whiteSpace: "pre-wrap",
-                      textAlign: "left",
-                    }}
-                  >
-                    {c.content}
-                  </p>
-                </Card>
-                <br />
-              </Col>
-            ))}
-        </Row>
+                />
+              </List.Item>
+            )}
+          />
+        )}
       </div>
       <Divider></Divider>
-
-      <List
-        // className="demo-loadmore-list"
-        itemLayout="horizontal"
-        bordered
-        style={{
-          marginTop: "80px",
-          minHeight: "400px",
-          width: "800px",
-          margin: "auto",
-        }}
-        dataSource={
-          // meme is not an array, so we need to convert it to an array
-          Object.keys(indexData)
-            .map((k: any) => indexData[k])
-            .slice(0, indexLimit)
-        }
-        renderItem={item => (
-          <List.Item
-            actions={[
-              <a key="list-loadmore-edit">{"Rank #" + item.market_cap_rank}</a>,
-              <a key="list-loadmore-more">{"Lqdty: " + Math.random().toFixed(2) + "P"}</a>,
-            ]}
-          >
-            <List.Item.Meta
-              avatar={
-                <Avatar
-                  style={{
-                    width: "50px",
-                    height: "50px",
-                  }}
-                  src={item.image}
-                />
-              }
-              title={
-                <a href="https://ant.design">
-                  {
-                    <>
-                      {item.name}
-                      <Tag
-                        style={{
-                          marginLeft: "10px",
-                        }}
-                      >
-                        {youngList.find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
-                          ?.networks?.[0].Name || "Wrapped Ethereum (Sepolia)"}
-                      </Tag>
-                    </>
-                  }
-                </a>
-              }
-              description={
-                <span>
-                  {youngList.find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
-                    ?.descriptions?.en !== undefined
-                    ? youngList
-                        .find((y: any) => String(y.symbol).toLowerCase() === item.symbol.toLowerCase())
-                        ?.descriptions?.en?.slice(0, 100) + "..."
-                    : "No description available"}
-                </span>
-              }
-            />
-          </List.Item>
-        )}
-      />
     </>
   );
 };
