c65626b84b3e1439c5584193357f3bc0b4fc63a6
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-07-06 10:44:39 +0100

    fix: last commit blockmagic

--- xtf/packages/nextjs/utils/scaffold-eth/contractsData.ts
+++ xtf/packages/nextjs/utils/scaffold-eth/contractsData.ts
@@ -5,3 +5,12 @@ export function getAllContracts() {
   const contractsData = contracts?.[scaffoldConfig.targetNetworks[0].id];
   return contractsData ? contractsData : {};
 }
+
+export const SEPOLIA_NETWOKR_ID = scaffoldConfig.targetNetworks[0].id;
+export const XRP_NETWORK_ID = scaffoldConfig.targetNetworks[1].id;
+
+
+export function getContractByNetworkId(network: number) {
+  const contractsData = contracts?.[network];
+  return contractsData ? contractsData : {};
+}
